1631742f9c94ea5f4d1bccb35402241a0d14ef0f
MFA571 <fakhar571@gmail.com>
2019-07-29 00:39:58 +0500

    added some parsing code

--- scrape_file.py
+++ scrape_file.py
@@ -1,3 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
+with open('sample.html') as html_file:
+    bsoup = BeautifulSoup(html_file, 'lxml')
+
+# print(bsoup.prettify())  # indents the code
+
+# match = bsoup.title.text  # only returns the first tag relevant to 'title'
+# print(match)
+
+# using find method to find exact tag you want
+
+match = bsoup.find('div', class_='footer') # using class_ to differentiate b/w python's class and bs
+
