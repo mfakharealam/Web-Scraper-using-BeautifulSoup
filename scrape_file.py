a631aacf2076ce8cee3cfad05c509932f42f19a8
MFA571 <fakhar571@gmail.com>
2019-07-29 17:24:02 +0500

    updated...

--- scrape_file.py
+++ scrape_file.py
@@ -4,16 +4,17 @@ import requests
 html_source = requests.get('https://coreyms.com').text
 soup = BeautifulSoup(html_source, 'lxml')
 # print(soup.prettify())
-article = soup.find('article')
-# headline = article.h2.a.text
-# print(headline)
-# content_div = soup.find('div', class_='entry-content')
-# content = content_div.p.text
-# print(content)
-video_src = article.find('iframe', 'youtube-player')['src']  # stored like a dictionary
-video_id = video_src.split('/')[4]
-video_id = video_id.split('?')[0]
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
+    content_div = soup.find('div', class_='entry-content')
+    content = content_div.p.text
+    print(content)
+    video_src = article.find('iframe', 'youtube-player')['src']  # stored like a dictionary
+    video_id = video_src.split('/')[4]
+    video_id = video_id.split('?')[0]
 
-# creating a youtube link
-yt_link = f'https://youtube.com/watch?v={video_id}'
-print(yt_link)
+    # creating a youtube link
+    yt_link = f'https://youtube.com/watch?v={video_id}'
+    print(yt_link)
+    print()     # empty line
